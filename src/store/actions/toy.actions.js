ca4b742055bc1d7297d20dbb5629757cdcbc1702
onexsus <boriskho@gmail.com>
2023-12-26 06:46:49 +0200

    done store toy

--- src/store/actions/toy.actions.js
+++ src/store/actions/toy.actions.js
@@ -1,1 +1,59 @@
 
+import { toyService } from "../../services/toy.service.js"
+import { ADD_TOY,REMOVE_TOY, SET_TOYS, SET_FILTER_BY, SET_IS_LOADING, UPDATE_TOY } from "../reducers/toy.reducer.js"
+import { store } from "../store.js"
+
+
+
+
+export function loadToys() {
+    store.dispatch({ type: SET_IS_LOADING, isLoading: true })
+    const filterBy = store.getState().toyModule.filterBy
+    return toyService.query(filterBy)
+        .then(toys => {
+            store.dispatch({ type: SET_TOYS, toys })
+        })
+        .catch(err => {
+            console.log('toy action -> Cannot load toys', err)
+            throw err
+        })
+        .finally(() => {
+            store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+        })
+}
+
+
+export function removeToy(toyId) {
+    store.dispatch({ type: SET_IS_LOADING, isLoading: true })
+    return toyService.remove(toyId)
+        .then(() => {
+            store.dispatch({ type: REMOVE_TOY, toyId })
+        })
+        .catch(err => {
+            console.log('toy action -> Cannot remove toy', err)
+            throw err
+        })
+        .finally(() => {
+            store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+        })
+}
+
+
+export function saveToy(toy) {
+    const type = toy._id ? UPDATE_TOY : ADD_TOY
+    return toyService.save(toy)
+        .then(toyToSave => {
+            store.dispatch({ type, toy: toyToSave })
+            return toyToSave
+        })
+        .catch(err => {
+            console.log('toy action -> Cannot save toy', err)
+            throw err
+        })
+}
+
+
+
+export function setFilterBy(filterBy) {
+    store.dispatch({ type: SET_FILTER_BY, filterBy })
+}
--- src/store/reducers/toy.reducer.js
+++ src/store/reducers/toy.reducer.js
@@ -3,22 +3,47 @@ import { toyService } from "../../services/toy.service.js"
 
 // toy
 export const SET_TOYS = 'SET_TOYS'
+export const REMOVE_TOY = 'REMOVE_TOY'
+export const ADD_TOY = 'ADD_TOY'
+export const UPDATE_TOY = 'UPDATE_TOY'
 
 
 export const SET_IS_LOADING = 'SET_IS_LOADING'
+export const SET_FILTER_BY = 'SET_FILTER_BY'
 
 const initialState = {
     toys: [],
     isLoading: false,
+    filterBy: toyService.getDefaultFilter()
 }
 
 export function toyReducer(state = initialState, action = {}) {
     let toys
+    let lastToys
     let shoppingCart
     switch (action.type) {
         // toy
         case SET_TOYS:
             return { ...state, toys: action.toys }
+        case REMOVE_TOY:
+            lastCars = [...state.cars]
+            cars = state.cars.filter(toy => toy._id !== action.toyId)
+            return { ...state, toys, lastToys }
+
+        case ADD_TOY:
+            toys = [...state.toys, action.toy]
+            return { ...state, toys }
+
+        case UPDATE_TOY:
+            toys = state.toys.map(toy => toy._id === action.toy._id ? action.toy : toy)
+            return { ...state, toys }
+
+        case SET_IS_LOADING:
+            return { ...state, isLoading: action.isLoading }
+
+        case SET_FILTER_BY:
+            return { ...state, filterBy: { ...state.filterBy, ...action.filterBy } }    
+
         default:
             return state
     }
